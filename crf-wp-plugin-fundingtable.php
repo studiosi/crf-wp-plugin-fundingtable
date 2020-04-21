f2ee0d3609e678eabf92135f79f9e2e27f79bf92
Studiosi <david@studiosi.es>
2020-04-21 20:33:32 +0300

    Initial commit: plugin header and license file

---
+++ crf-wp-plugin-fundingtable.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Crowdfight Covid-19 Funding Table
+ * Description: Provides a shortcode to render the funding table
+ * Version: 0.1
+ * Author: The Crowdfight Covid-19 Team
+ * Author URI: https://crowdfightcovid19.org/
+ * License: GPLv2
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
